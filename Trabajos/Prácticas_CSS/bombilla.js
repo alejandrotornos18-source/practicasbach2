8b12965e4ee456ffdbe447c7ee08d3df6da6372d
alejandrotornos18-source <alejandro.tornos.18@monzon.salesianos.org>
2025-10-31 09:56:47 +0100

    Add files via upload

---
+++ Trabajos/Prácticas_CSS/bombilla.js
@@ -0,0 +1,10 @@
+function Encender() {
+	document.getElementById("bombilla").src= "on.jpg"; // imagen encendida
+	document.getElementById("estado").textContent = "La bombilla esta encendida";
+}
+
+//Funcion para apagar la bombilla
+function Apagar() {
+	document.getElementById("bombilla").src= "off.jpg"; // imagen apagada
+	document.getElementById("estado").textContent = "La bombilla esta apagada";
+}
